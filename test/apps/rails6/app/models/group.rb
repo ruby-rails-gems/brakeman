8a5399b54b6e91e6071cb684fac6f3b1777993bf
Justin Collins <justin@presidentbeef.com>
2021-02-17 21:41:03 -0800

    Very simplest return values

--- test/apps/rails6/app/models/group.rb
+++ test/apps/rails6/app/models/group.rb
@@ -2,4 +2,12 @@ class Group < ApplicationRecord
   def uuid_in_sql
     ActiveRecord::Base.connection.exec_query("select * where x = #{User.uuid}")
   end
+
+  def use_simple_method
+    self.where("thing = #{Group.simple_method}")
+  end
+
+  def self.simple_method
+    "Hello"
+  end
 end
